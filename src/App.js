1cd778c33985d7aa8fb5dc733579d0843180d60f
Anastasiya Turenko <oxtnpi@gmail.com>
2021-10-07 16:14:20 +0600

    add deps

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import { useFetchRoutes } from './hooks/useFetchRoutes';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
 import logo from './logo.svg';
 import './App.css';
@@ -12,20 +13,12 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </MapContainer>
     </div>
   );
 }
